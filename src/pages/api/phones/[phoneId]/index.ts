b3ff024f027fff874d18196b21d5f76819dc149f
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-01-24 00:17:43 +0100

    feat: new 'PATCH /api/phones/[phoneId]'

--- src/pages/api/phones/[phoneId]/index.ts
+++ src/pages/api/phones/[phoneId]/index.ts
@@ -1,7 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
+import { ValidationError } from 'yup';
 
-import { deletePhone, getPhone } from '../../../../lib/data/phone';
+import { deletePhone, getPhone, updatePhone } from '../../../../lib/data/phone';
 
 const handleGetPhone = nc()
 	.get(async (req: NextApiRequest, res: NextApiResponse) => {
@@ -58,6 +59,51 @@ const handleGetPhone = nc()
 		}
 
 		res.status(204).end();
+	})
+	.patch(async (req: NextApiRequest, res: NextApiResponse) => {
+		if (!req?.query?.phoneId) {
+			res.status(400).json({
+				message: 'Missing phoneId in query',
+			});
+			res.end();
+			return;
+		}
+
+		if (typeof req.query.phoneId !== 'string') {
+			res.status(400).json({
+				message: 'Invalid phoneId type',
+			});
+			res.end();
+			return;
+		}
+
+		try {
+			const { data } = await updatePhone({ id: req.query.phoneId, ...req.body });
+
+			if (!data) {
+				res.status(404);
+				res.end();
+				return;
+			}
+
+			res.status(200).json({ data });
+			res.end();
+		} catch (error) {
+			if (error instanceof ValidationError) {
+				res.status(400).json({ error: { message: error.message } });
+				res.end();
+				return;
+			}
+
+			if (error instanceof Error) {
+				res.status(500).json({ error: { message: error.message } });
+				res.end();
+				return;
+			}
+
+			res.status(500).json({ error: { message: 'Unknown error' } });
+			res.end();
+		}
 	});
 
 export default handleGetPhone;
--- test/integration/pages/api/phones/{phoneId}/index.spec.ts
+++ test/integration/pages/api/phones/{phoneId}/index.spec.ts
@@ -78,5 +78,70 @@ withDb(() => {
 				expect(response.status).toBe(204);
 			});
 		});
+
+		describe('PATCH /api/phones/[phoneId]', () => {
+			it('returns a 400 error if the phone id is missing', async () => {
+				setQuery({ phoneId: undefined });
+				const response = await fetch(getUrl(), {
+					method: 'PATCH',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ name: 'iPhone' }),
+				});
+
+				expect(response.status).toBe(400);
+				expect(await response.json()).toStrictEqual({ message: 'Missing phoneId in query' });
+			});
+
+			it('returns a 400 error if the phone id is an array', async () => {
+				setQuery({ phoneId: [] });
+				const response = await fetch(getUrl(), {
+					method: 'PATCH',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ name: 'iPhone' }),
+				});
+				expect(response.status).toBe(400);
+				expect(await response.json()).toStrictEqual({ message: 'Invalid phoneId type' });
+			});
+
+			it('returns a 404 error if the phone does not exist in the db', async () => {
+				const phone = new PhoneBuilder().get();
+				setQuery({ phoneId: phone.id });
+
+				const response = await fetch(getUrl(), {
+					method: 'PATCH',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ name: 'iPhone' }),
+				});
+
+				expect(response.status).toBe(404);
+			});
+
+			it('returns a 400 error if the user tries to update the phone with invalid data', async () => {
+				const phone = await new PhoneBuilder().save();
+				setQuery({ phoneId: phone.id });
+
+				const response = await fetch(getUrl(), {
+					method: 'PATCH',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ name: null }),
+				});
+
+				expect(response.status).toBe(400);
+				expect(await response.json()).toEqual({ error: { message: expect.any(String) } });
+			});
+
+			it('returns a 200 if the phone exists and was updated', async () => {
+				const phone = await new PhoneBuilder().save();
+				setQuery({ phoneId: phone.id });
+				const response = await fetch(getUrl(), {
+					method: 'PATCH',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ name: 'iPhone' }),
+				});
+
+				expect(response.status).toBe(200);
+				expect(await response.json()).toMatchObject({ data: { id: phone.id, name: 'iPhone' } });
+			});
+		});
 	});
 });
