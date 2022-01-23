7af4de65eae40bfdc734fc9b0c9966018ace43d6
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-01-23 23:55:42 +0100

    feat: new 'POST /api/phone' endpoint

--- src/pages/api/phones/index.ts
+++ src/pages/api/phones/index.ts
@@ -1,11 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
+import { ValidationError } from 'yup';
 
-import { getPhones } from '../../../lib/data/phone';
+import { createPhone, getPhones } from '../../../lib/data/phone';
 
-const handleGetPhones = nc().get(async (_: NextApiRequest, res: NextApiResponse) => {
-	res.status(200).json(await getPhones());
-	res.end();
-});
+const handleGetPhones = nc()
+	.get(async (_: NextApiRequest, res: NextApiResponse) => {
+		res.status(200).json(await getPhones());
+		res.end();
+	})
+	.post(async (req: NextApiRequest, res: NextApiResponse) => {
+		try {
+			const { data: phone } = await createPhone(req.body);
+
+			res.status(201).json({ data: phone });
+			res.end();
+			return;
+		} catch (error) {
+			if (error instanceof ValidationError) {
+				res.status(400).json({ error: { message: error.message } });
+				res.end();
+				return;
+			}
+
+			if (error instanceof Error) {
+				res.status(500).json({ error: { message: error.message } });
+				res.end();
+				return;
+			}
+
+			res.status(500).json({ error: { message: 'Unknown error' } });
+			res.end();
+		}
+	});
 
 export default handleGetPhones;
--- test/integration/pages/api/phones/index.spec.ts
+++ test/integration/pages/api/phones/index.spec.ts
@@ -21,5 +21,31 @@ withDb(() => {
 				expect(await response.json()).toEqual({ data: expect.arrayContaining(dbPhones) });
 			});
 		});
+
+		describe('POST /api/phones', () => {
+			it('returns a 400 error if the phone is missing any field', async () => {
+				const phone = new PhoneBuilder({ name: undefined }).get();
+				const response = await fetch(getUrl(), {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify(phone),
+				});
+
+				expect(response.status).toBe(400);
+				expect(await response.json()).toEqual({ error: { message: 'name is a required field' } });
+			});
+
+			it('returns a 201 and the created phone if everything goes well', async () => {
+				const phone = new PhoneBuilder().get();
+				const response = await fetch(getUrl(), {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify(phone),
+				});
+
+				expect(response.status).toBe(201);
+				expect(await response.json()).toEqual({ data: phone });
+			});
+		});
 	});
 });
